3f0d1b661d99185bcbff60b6100a75100cef76c6
Cindy <seetheworld510306@gmail.com>
2023-06-22 16:52:44 +0800

    1652

--- script/teama_block.js
+++ script/teama_block.js
@@ -1,5 +1,8 @@
 const hamburger = document.querySelector(".navbar_hamburger");
 const navbar_menu = document.querySelector(".navbar_menu");
+const ref_click = document.querySelector(".ref_click");
+const ref_list = document.querySelector(".ref_list");
+
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
@@ -10,3 +13,12 @@ document.querySelectorAll(".navbar_link").forEach(n => n.addEventListener("click
     hamburger.classList.remove("active");
     navbar_menu.classList.remove("active");
 }));
+
+
+function book_click(){
+    ref_click.addEventListener("click", () => {
+        ref_click.classList.toggle("active");
+        ref_list.classList.toggle("active");
+    })
+
+}
