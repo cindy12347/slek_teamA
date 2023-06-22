b1e7fe13a2dd71003dd52a1cc42e6135b8a27803
Cindy <seetheworld510306@gmail.com>
2023-06-22 17:51:45 +0800

    1751

--- script/teama_block.js
+++ script/teama_block.js
@@ -1,7 +1,12 @@
 const hamburger = document.querySelector(".navbar_hamburger");
 const navbar_menu = document.querySelector(".navbar_menu");
+const navbar_link = document.querySelectorAll(".navbar_link");
 const ref_click = document.querySelector(".ref_click");
 const ref_list = document.querySelector(".ref_list");
+const wrong = document.querySelectorAll(".wrong");
+const right = document.querySelector(".right");
+const answer = document.querySelectorAll(".right, .wrong");
+
 
 
 hamburger.addEventListener("click", () => {
@@ -9,16 +14,19 @@ hamburger.addEventListener("click", () => {
     navbar_menu.classList.toggle("active");
 })
 
-document.querySelectorAll(".navbar_link").forEach(n => n.addEventListener("click", ()=> {
+navbar_link.forEach(n => n.addEventListener("click", ()=> {
     hamburger.classList.remove("active");
     navbar_menu.classList.remove("active");
 }));
-
 
 function book_click(){
     ref_click.addEventListener("click", () => {
         ref_click.classList.toggle("active");
         ref_list.classList.toggle("active");
     })
-
 }
+answer.forEach(n => n.addEventListener("click", ()=> {
+    right.classList.toggle("active");
+    wrong.forEach(n => n.classList.toggle("active"));
+    
+}));
