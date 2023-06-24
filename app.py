0941cc0dea07e469b0236370f2a79caf83c47163
Cindy <seetheworld510306@gmail.com>
2023-06-24 17:05:10 +0800

    1635

--- app.py
+++ app.py
@@ -2,11 +2,19 @@ from os import path
 from pathlib import Path
 
 from flask import Flask, render_template, request
+from flask_frozen import Freezer
 
 
 template_folder = path.abspath('./templates')
 
 app = Flask(__name__, template_folder=template_folder)
+#app.config['FREEZER_BASE_URL'] = environ.get('CI_PAGES_URL')
+app.config['FREEZER_DESTINATION'] = 'public'
+app.config['FREEZER_RELATIVE_URLS'] = True
+app.config['FREEZER_IGNORE_MIMETYPE_WARNINGS'] = True
+
+freezer = Freezer(app)
+
 
 @app.route('/')
 def index():
@@ -19,4 +27,5 @@ def pages(page):
 
 # Main Function, Runs at http://0.0.0.0:8080
 if __name__ == "__main__":
-    app.run(port=9090)
+    app.run(port=9090, debug=True)
+    freezer.freeze()
