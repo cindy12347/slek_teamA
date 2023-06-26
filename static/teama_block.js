68bc10bbf9a895fd510502c638d9c6a43d5cddad
Cindy <seetheworld510306@gmail.com>
2023-06-27 01:20:44 +0800

    0120

--- static/teama_block.js
+++ static/teama_block.js
@@ -1,19 +1,24 @@
 const hamburger = document.querySelector(".navbar_hamburger");
+const arrow = document.querySelector(".navbar_arrow");
 const navbar_menu = document.querySelector(".navbar_menu");
 const navbar_link = document.querySelectorAll(".navbar_link");
 const ref_click = document.querySelector(".ref_click");
 const ref_list = document.querySelector(".ref_list");
+
 const wrong2 = document.querySelectorAll(".wrong2");
 const right2 = document.querySelector(".right2");
 const wrong5 = document.querySelectorAll(".wrong5");
 const right5 = document.querySelector(".right5");
-const additional_block = document.querySelector(".additional_block");
-const additional = document.querySelector("#additional");
 const button2 = document.querySelectorAll(".wrong2, .right2");
 const button5 = document.querySelectorAll(".wrong5, .right5");
+
+const additional_block = document.querySelector(".additional_block");
+const additional = document.querySelector("#additional");
+
 const diagnosis_2button_block = document.querySelector(".diagnosis_2button_block");
 const diagnosis_5button_block = document.querySelector(".diagnosis_5button_block");
-
+const answer2 = document.querySelector("#answer2");
+const answer5 = document.querySelector("#answer5");
 
 
 hamburger.addEventListener("click", () => {
@@ -39,9 +44,13 @@ button2.forEach(n => n.addEventListener("click", ()=> {
     right2.disabled = true;
     wrong2.forEach(n => n.disabled = true);
 
+    
     diagnosis_2button_block.style.display = "none";
     diagnosis_5button_block.style.display = "flex";
 }))
+
+
+
 
 button5.forEach(n => n.addEventListener("click", ()=> {
     right5.classList.toggle("active");
