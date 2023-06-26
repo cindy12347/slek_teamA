7c18005d775092609965e0b6fdfcdada3744efac
Cindy <seetheworld510306@gmail.com>
2023-06-26 22:42:31 +0800

    1042

--- static/teama_block.js
+++ static/teama_block.js
@@ -3,9 +3,16 @@ const navbar_menu = document.querySelector(".navbar_menu");
 const navbar_link = document.querySelectorAll(".navbar_link");
 const ref_click = document.querySelector(".ref_click");
 const ref_list = document.querySelector(".ref_list");
-const wrong = document.querySelectorAll(".wrong");
-const right = document.querySelector(".right");
-const answer = document.querySelectorAll(".right, .wrong");
+const wrong2 = document.querySelectorAll(".wrong2");
+const right2 = document.querySelector(".right2");
+const wrong5 = document.querySelectorAll(".wrong5");
+const right5 = document.querySelector(".right5");
+const additional_block = document.querySelector(".additional_block");
+const additional = document.querySelector("#additional");
+const button2 = document.querySelectorAll(".wrong2, .right2");
+const button5 = document.querySelectorAll(".wrong5, .right5");
+const diagnosis_2button_block = document.querySelector(".diagnosis_2button_block");
+const diagnosis_5button_block = document.querySelector(".diagnosis_5button_block");
 
 
 
@@ -25,13 +32,24 @@ function book_click(){
         ref_list.classList.toggle("active");
     })
 }
-answer.forEach(n => n.addEventListener("click", ()=> {
-    right.classList.toggle("active");
-    wrong.forEach(n => n.classList.toggle("active"));
-    right.disabled = true;
-    wrong.forEach(n => n.disabled = true);
 
-    
-}));
+button2.forEach(n => n.addEventListener("click", ()=> {
+    right2.classList.toggle("active");
+    wrong2.forEach(n => n.classList.toggle("active"));
+    right2.disabled = true;
+    wrong2.forEach(n => n.disabled = true);
 
+    diagnosis_2button_block.style.display = "none";
+    diagnosis_5button_block.style.display = "flex";
+}))
+
+button5.forEach(n => n.addEventListener("click", ()=> {
+    right5.classList.toggle("active");
+    wrong5.forEach(n => n.classList.toggle("active"));
+    right5.disabled = true;
+    wrong5.forEach(n => n.disabled = true);
+
+    additional_block.style.display = "flex";
+    $('html, body').animate({scrollTop: $('#additional').offset().top});
+}))
 
